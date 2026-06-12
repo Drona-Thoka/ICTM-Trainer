21ae0707cca05d357468f6477494ae55b24931bd
Avi Tyagi <aviatray@icloud.com>
2026-06-12 15:24:00 -0500

    penis

--- ictm-reader/src/App.tsx
+++ ictm-reader/src/App.tsx
@@ -54,15 +54,6 @@ function App() {
         <Link to="/comp-2" className="nav-button">
           Comp 2
         </Link>
-        <Link to="/comp-3" className="nav-button">
-          Comp 3
-        </Link>
-        <Link to="/comp-4" className="nav-button">
-          Comp 4
-        </Link>
-        <Link to="/comp-5" className="nav-button">
-          Comp 5
-        </Link>
       </nav>
 
       <Routes>
@@ -74,18 +65,6 @@ function App() {
         <Route
           path="/comp-2"
           element={<CompPage title="Component 2" description="This is the page for Component 2." />}
-        />
-        <Route
-          path="/comp-3"
-          element={<CompPage title="Component 3" description="This is the page for Component 3." />}
-        />
-        <Route
-          path="/comp-4"
-          element={<CompPage title="Component 4" description="This is the page for Component 4." />}
-        />
-        <Route
-          path="/comp-5"
-          element={<CompPage title="Component 5" description="This is the page for Component 5." />}
         />
       </Routes>
     </div>
