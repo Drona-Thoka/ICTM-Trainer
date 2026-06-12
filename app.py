ffee516dd33525bfb5d8931b791ae3ee42d05707
titan00000001 <92939107+Shadow8928@users.noreply.github.com>
2026-06-12 13:30:31 -0500

    skeleton backend

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-@app.route("/get_amc")
+
+@app.route("/get_amc/<diff>")
 def get_amc(diff:str):
     return "<p>Hello, World!</p>"
-@app.route("/get_ictm")
+
+@app.route("/get_ictm/<diff>")
 def get_ictm(diff:str):
     return "<p>Hello, World!</p>"
-@app.route("/get_nsml")
+
+@app.route("/get_nsml/<diff>")
 def get_nsml(diff:str):
     return "<p>Hello, World!</p>"
-@app.route("/get_aime")
+
+@app.route("/get_aime/<diff>")
 def get_aime(diff:str):
     return "<p>Hello, World!</p>"
-@app.route("/get_arml")
+
+@app.route("/get_arml/<diff>")
 def get_arml(diff:str):
     return "<p>Hello, World!</p>"
