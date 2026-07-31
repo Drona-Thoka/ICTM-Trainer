082449a7ac86bb2a5c97f2b4740452ae3ac630f4
Avi Tyagi <aviatray0708@gmail.com>
2026-07-31 14:50:39 -0500

    fixed all latex errors

--- ictm-reader/src/App.tsx
+++ ictm-reader/src/App.tsx
@@ -9,7 +9,7 @@ import { ALL_EVENTS, ALL_LEVELS, ALL_TOPICS, useIctmEvents, useTopics } from './
 import { supabase } from './supabaseClient'
 import { ThemeProvider, useTheme } from './ThemeContext'
 
-const APP_VERSION = '1.0.2'
+const APP_VERSION = '1.0.3'
 
 // ---- API types (see ICTM-Trainer/app.py) ---------------------------------
 
